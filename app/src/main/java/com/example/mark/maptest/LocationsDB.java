57d90258c410ca72a82ae6b2b65de5aeb0ca4039
Mark <gerrity95@gmail.com>
2016-06-28 17:24:11 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/mark/maptest/LocationsDB.java
@@ -0,0 +1,29 @@
+package com.example.mark.maptest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Mark on 12/06/2016.
+ * Used for creating a table class
+ */
+public class LocationsDB extends SQLiteOpenHelper {
+
+
+    public LocationsDB(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
